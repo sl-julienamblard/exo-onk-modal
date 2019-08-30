54ccf0b630af85f4a09d3d13e962324b83b7a9e3
Stefan <stefan.lentner@gmail.com>
2019-08-30 16:05:32 +0200

    feat:exo-benjamin-budet

--- src/Modal/index.js
+++ src/Modal/index.js
@@ -1,5 +1,38 @@
 import React from "react";
+import { Button } from "@ornikar/kitt";
+import styles from "./styles.module.css";
 
-export default function Modal() {
-  return <div>TODO</div>;
+function Header({ title, onClose }) {
+  return (
+    <div className={styles.ModalHeader}>
+      <p className={styles.ModalHeaderTitle}>{title}</p>
+      <Button icon="cross" onClick={onClose} />
+    </div>
+  );
+}
+
+function Content({ children }) {
+  return <div className={styles.ModalContent}>{children}</div>;
+}
+
+function Footer({ onApply }) {
+  return (
+    <div className={styles.ModalFooter}>
+      <Button type="primary" onClick={onApply}>
+        Appliquer
+      </Button>
+    </div>
+  );
+}
+
+export default function Modal({ isOpen, title, onClose, children }) {
+  if (isOpen)
+    return (
+      <div className={styles.Modal}>
+        <Header title={title} onClose={onClose} />
+        <Content>{children}</Content>
+        <Footer onApply={onClose} />
+      </div>
+    );
+  return null;
 }
--- src/ModalLaucher/index.js
+++ src/ModalLaucher/index.js
@@ -1,13 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "@ornikar/kitt";
+import Modal from "../Modal";
 
 export default function ModalLaucher() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <Button type="primary">
-      Show Modal{" "}
-      <span role="img" aria-label="so cool">
-        😎
-      </span>
-    </Button>
+    <>
+      <Button type="primary" onClick={() => setIsOpen(!isOpen)}>
+        Show Modal{" "}
+        <span role="img" aria-label="so cool">
+          😎
+        </span>
+      </Button>
+      <Modal isOpen={isOpen} title={"Titre"} onClose={() => setIsOpen(false)}>
+        <p>
+          Reference site about Lorem Ipsum, giving information on its origins,
+          as well as a random Lipsum generator.
+        </p>
+        <p>
+          Reference site about Lorem Ipsum, giving information on its origins,
+          as well as a random Lipsum generator.
+        </p>
+        <p>
+          Reference site about Lorem Ipsum, giving information on its origins,
+          as well as a random Lipsum generator.
+        </p>
+        <p>
+          Reference site about Lorem Ipsum, giving information on its origins,
+          as well as a random Lipsum generator.
+        </p>
+        <p>
+          Reference site about Lorem Ipsum, giving information on its origins,
+          as well as a random Lipsum generator.
+        </p>
+        <p>
+          Reference site about Lorem Ipsum, giving information on its origins,
+          as well as a random Lipsum generator.
+        </p>
+        <p>
+          Reference site about Lorem Ipsum, giving information on its origins,
+          as well as a random Lipsum generator.
+        </p>
+        <p>
+          Reference site about Lorem Ipsum, giving information on its origins,
+          as well as a random Lipsum generator.
+        </p>
+        <p>
+          Reference site about Lorem Ipsum, giving information on its origins,
+          as well as a random Lipsum generator.
+        </p>
+      </Modal>
+    </>
   );
 }
